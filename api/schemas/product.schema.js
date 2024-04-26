7cff29d06c1131e3dbba9c677afd299f5a62b967
doorme777 <137806586+doorme777@users.noreply.github.com>
2024-04-26 11:05:30 -0600

    problemas con el joi solucionados

--- api/schemas/product.schema.js
+++ api/schemas/product.schema.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 const id = Joi.string().uuid();
-const name = Joi.string().string().min(3).max(15);
+const name = Joi.string().min(3).max(15);
 const price = Joi.number().integer().min(10);
 const image = Joi.string().uri();
 
